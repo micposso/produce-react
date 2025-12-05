49c7deca6383bdb90ecb430a529a0a000e01a398
micposso <micpsso@gmail.com>
2025-12-04 22:18:20 -0500

    configure gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/produce-react/',   // https://username.github.io/REPO_NAME/
+  plugins: [react()]
 })
